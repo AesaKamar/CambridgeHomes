947ed2287d8e6777cba68c1742559872d4ca5f1d
Aesa Kamar <Aesa.Kamar@Gmail.com>
2016-04-11 12:11:32 -0400

    Added documentation to UsersController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,11 +6,21 @@
  */
 
 module.exports = {
+  //ACTION NAME
   create: function(req, res) {
+    //This is what the action will do
+    //Creates a User in the database based on
     var params = req.params.all();
     User.create({
       email: params.email
-    }).exec(function createCB(err, created) {
+    })
+    //CALLBACK ON SUCCESS OR FAIL
+    //err contains the error messages if action failed
+    //
+    //created, or whatever we decide to call it, contains
+    //The object that was returned by the action, in this case
+    //it is the user that we returned from the User.create() call
+    .exec(function createCB(err, created) {
       return res.json({
         notice: created || err
       });
